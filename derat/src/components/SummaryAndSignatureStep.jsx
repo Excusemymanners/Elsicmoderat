c390f4592f411a1c8cfdf185184edf0bf5debf62
flcristian <qflorescucristian@gmail.com>
2025-02-12 23:31:47 +0200

    fix: fixed send email

--- derat/src/components/SummaryAndSignatureStep.jsx
+++ derat/src/components/SummaryAndSignatureStep.jsx
@@ -168,7 +168,7 @@ const SummaryAndSignatureStep = () => {
     let customerEmail = formData.customer.email;
 
     try {
-      const response = await fetch('http://localhost:5000/api/send-email', {
+      const response = await fetch('/api/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
