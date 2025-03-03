804ad4374628d9ac08067f0d570b5f01b1aa49fd
Excusemymanners <lucachristian.pilat@ulbsibiu.ro>
2025-03-03 11:55:30 +0200

    lol

--- derat/src/components/fillTemplate.jsx
+++ derat/src/components/fillTemplate.jsx
@@ -109,9 +109,9 @@ export const fillTemplate = async (templateUrl, request) => {
       let yPosition = 365 - coordinate * 22;
       drawText('X', 130, yPosition);
 
-      const surfaceXPosition = 182;
+      const surfaceXPosition = 180;
       const solutionXPosition = 250;
-      const quantityXPosition = solutionXPosition + 120;
+      const quantityXPosition = solutionXPosition + 150;
       const concentrationXPosition = quantityXPosition + 105;
       const lotXPosition = concentrationXPosition + 135;
 
