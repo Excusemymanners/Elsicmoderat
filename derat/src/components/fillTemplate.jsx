8437e842afff51008975b54a13cca21a36d81d46
Excusemymanners <lucachristian.pilat@ulbsibiu.ro>
2025-03-05 09:44:54 +0200

    fillTemplate

--- derat/src/components/fillTemplate.jsx
+++ derat/src/components/fillTemplate.jsx
@@ -130,7 +130,7 @@ export const fillTemplate = async (templateUrl, request) => {
       const solutionXPosition = 250;
       const quantityXPosition = solutionXPosition + 145;
       const concentrationXPosition = quantityXPosition + 140;
-      const lotXPosition = concentrationXPosition + 65;
+      const lotXPosition = concentrationXPosition + 70;
 
       drawText(`${operation.surface}mp`, surfaceXPosition, yPosition);
       drawText(`${operation.solution}`, solutionXPosition, yPosition);
@@ -138,7 +138,7 @@ export const fillTemplate = async (templateUrl, request) => {
       drawText(`${operation.concentration}%`, concentrationXPosition, yPosition);
       drawText(`${operation.lot}`, lotXPosition, yPosition);
     }
-    drawText(`${request.observations}`, 210, 300);
+    drawText(`${request.observations}`, 210, 550);
 
     console.log(request.observations)
     // Save the PDF document and return the bytes
