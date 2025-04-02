4698259b9b4c29f654e3845cadb64cc912f9edf2
Excusemymanners <lucachristian.pilat@ulbsibiu.ro>
2025-04-02 21:29:31 +0300

    xwex

--- derat/src/components/fillTemplate.jsx
+++ derat/src/components/fillTemplate.jsx
@@ -156,7 +156,7 @@ export const fillTemplate = async (templateUrl, request) => {
     custodyItems.forEach(item => {
       if (item.value > 0) {
         drawText(`${item.label}: ${item.value}`, xOffset, height - 390);
-        xOffset += 300;
+        xOffset =xOffset+ 300;
       }
     });
 
